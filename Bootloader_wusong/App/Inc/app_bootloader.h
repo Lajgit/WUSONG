8515c79c35a9cb4c3c02359e86a9d5e9111cc957
Lajgit <3243570032@qq.com>
2026-07-03 12:14:31 +0800

    feat: add Wusong OTA core modules

--- Bootloader_wusong/App/Inc/app_bootloader.h
+++ Bootloader_wusong/App/Inc/app_bootloader.h
@@ -23,9 +23,23 @@
 
 #define OTA_CACHE_ADDR 0x080A0000U
 #define OTA_META_ADDR 0x080DFF00U
+#define OTA_META_VALID_ADDR OTA_META_ADDR
+#define OTA_META_INSTALLING_MARKER_ADDR (OTA_META_ADDR + 0x20U)
+#define OTA_META_INSTALLED_MARKER_ADDR (OTA_META_ADDR + 0x24U)
 #define OTA_CACHE_MAX_SIZE (OTA_META_ADDR - OTA_CACHE_ADDR)
 
-#define OTA_TARGET_MAGIC 0x41544F42U
+#define OTA_META_MARKER_EMPTY 0xFFFFFFFFU
+#define OTA_META_MARKER_SET 0x00000000U
+
+#define OTA_REQUEST_MAGIC 0x424F5441U
+#define OTA_TARGET_MAGIC 0x41544F42U /* BEGIN中的字节42 4F 54 41，小端读取 */
+#define OTA_METADATA_MAGIC 0x4D41544FU
+
+#define OTA_META_EMPTY 0xFFFFFFFFU
+#define OTA_META_VALID 0xFFFFFFFEU
+#define OTA_META_INSTALLING 0xFFFFFFFCU
+#define OTA_META_INSTALLED 0xFFFFFFF8U
+
 #define OTA_SOF1 0xAAU
 #define OTA_SOF2 0x5AU
 #define OTA_PROTOCOL_VERSION 0x01U
@@ -67,7 +81,31 @@
 #define OTA_SESSION_INSTALLED 4U
 #define OTA_SESSION_ERROR 5U
 
+typedef struct
+{
+    uint32_t magic;
+    uint32_t state;
+    uint32_t version_code;
+    uint32_t image_size;
+    uint32_t image_crc32;
+    uint32_t header_crc32;
+} OtaMetadata_t;
+
 HAL_StatusTypeDef Flash_Program(uint32_t StartAddress, uint8_t *Data, uint32_t Size);
+HAL_StatusTypeDef Boot_FlashWrite(uint32_t start_address, const uint8_t *data, uint32_t size);
+HAL_StatusTypeDef Boot_EraseCache(void);
+bool Boot_FlashCompare(uint32_t address, const uint8_t *data, uint32_t size);
+bool Boot_AppIsValid(void);
+bool Boot_CachedImageVectorIsValid(uint32_t image_size);
+bool Boot_ConsumeOtaRequest(void);
+bool Boot_ReadMetadata(OtaMetadata_t *metadata);
+HAL_StatusTypeDef Boot_WriteMetadata(uint32_t version_code, uint32_t image_size, uint32_t image_crc32);
+HAL_StatusTypeDef Boot_SetMetadataState(uint32_t state);
+HAL_StatusTypeDef Boot_InstallCachedImage(void);
+void Boot_LightWaiting(void);
+void Boot_LightSuccess(void);
+void Boot_LightError(void);
+void OTA_Run(void);
 void JumpToApplication(void);
 void App_Bootloader(void);
 
